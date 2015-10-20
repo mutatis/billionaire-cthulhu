0d1c164643b69c3f8d0ee6125b9689fa8db25519
Gabriel <mutatisguga@gmail.com>
2015-10-20 10:19:31 -0200

    d

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using SIS;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager manager;
+
+    public int money = DBManager.GetFunds("coins");
+
+    void Awake()
+    {
+        manager = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        money = DBManager.GetFunds("coins");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        money = DBManager.GetFunds("coins");
+        print(money);
+    }
+}
---
+++ Assets/Script/Manager/Joga.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SIS;
+
+public class Joga : MonoBehaviour {
+
+	float mouseY;
+
+    bool creat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		mouseY = Input.GetAxis("Mouse Y");
+
+		if(Input.GetMouseButton(0) && mouseY > 0)
+		{
+            creat = true;
+			Debug.Log(mouseY);
+		}
+
+        if(creat && Input.GetMouseButtonUp(0) && GameManager.manager.money > 0)
+        {
+            print("Cria");
+            DBManager.IncreaseFunds("coins", -1); //depois tem q mandar diminuir do DBManager do SIS
+            creat = false;
+        }
+	}
+}
