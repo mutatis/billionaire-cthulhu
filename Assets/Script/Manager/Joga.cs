ac7997a6241f9f6e686b607fca5dc0c6ff0c0a57
Gabriel <mutatisguga@gmail.com>
2015-11-04 14:18:37 -0200

    f

--- Assets/Script/Manager/Joga.cs
+++ Assets/Script/Manager/Joga.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using SIS;
 
-public class Joga : MonoBehaviour {
+public class Joga : MonoBehaviour
+{
+    public GameObject obj;
 
+    public float positionY;
+
+    float[] positionX = { -5, 5 };
 	float mouseY;
 
     bool creat;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    void Start()
+    {
+        DBManager.IncreaseFunds("coins", 10000);
+    }
+
 	void Update ()
     {
 		mouseY = Input.GetAxis("Mouse Y");
@@ -21,13 +25,14 @@ public class Joga : MonoBehaviour {
 		if(Input.GetMouseButton(0) && mouseY > 0)
 		{
             creat = true;
-			Debug.Log(mouseY);
 		}
 
         if(creat && Input.GetMouseButtonUp(0) && GameManager.manager.money > 0)
         {
-            print("Cria");
+            var x = Random.Range(positionX[0], positionX[1]);
+
             DBManager.IncreaseFunds("coins", -1); //depois tem q mandar diminuir do DBManager do SIS
+            Instantiate(obj, new Vector3(x, positionY, 0), transform.rotation);
             creat = false;
         }
 	}
---
+++ Assets/Script/Money/Money.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Money : MonoBehaviour
+{
+
+    public Rigidbody2D rig;
+
+    public float impulso;
+
+    bool jump;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!jump || Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.velocity = new Vector3(0, impulso, 0);
+            jump = true;
+        }
+	}
+}
